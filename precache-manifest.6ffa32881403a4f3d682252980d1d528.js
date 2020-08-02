9ca7d792c6ddbdb769b9247c785a9cdcd700f10b
rohansharma06 <rohansharma251998@gmail.com>
2020-08-02 12:46:01 +0530

    Updates

---
+++ precache-manifest.6ffa32881403a4f3d682252980d1d528.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67b75fed9afb87f17165e27c3e4bfe9",
+    "url": "/iPod-app/index.html"
+  },
+  {
+    "revision": "af4ea5ca6cbbfccd597c",
+    "url": "/iPod-app/static/css/main.f0bf4e65.chunk.css"
+  },
+  {
+    "revision": "e7a9f353d2267beb4f48",
+    "url": "/iPod-app/static/js/2.f87b9c93.chunk.js"
+  },
+  {
+    "revision": "3b6069e01f0db714f9e787b4dbe9775c",
+    "url": "/iPod-app/static/js/2.f87b9c93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af4ea5ca6cbbfccd597c",
+    "url": "/iPod-app/static/js/main.8fe0cc99.chunk.js"
+  },
+  {
+    "revision": "b4e27c359a03df3e1c62",
+    "url": "/iPod-app/static/js/runtime-main.54037b8d.js"
+  }
+]);
